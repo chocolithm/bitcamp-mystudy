96e00060f1f03e2ca20d1697ba8645f2ed92b856
algo-itthm <kys8336@gmail.com>
2024-06-03 18:17:42 +0900

    2024-06-03

---
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -0,0 +1,11 @@
+package study.lang.literal;
+
+// 실습
+// - 12.375f 부동소수점을 출력하라.
+// - 컴파일된 클래스 파일을 16진수 코드로 읽어서 12.375가 2진수로 저장된 것을 확인하라.. 
+// 
+public class Test06 {
+    public static void main(String[] args) {
+        System.out.println(12.375f); // 부동소수점이 메모리에 저장될 때 2진수로 바귀는 것을 확인한다.
+    }
+}
