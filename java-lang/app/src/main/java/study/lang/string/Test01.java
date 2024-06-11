dce1624e671c536ccae2f1d12d790171744672b1
algo-itthm <kys8336@gmail.com>
2024-06-11 17:50:41 +0900

    2024-06-11

---
+++ java-lang/app/src/main/java/study/lang/string/Test01.java
@@ -0,0 +1,12 @@
+package study.lang.string;
+
+public class Test01 {
+  public static void main(String[] args) {
+    String s;
+    s = new String("aaa");
+
+    String s2 = new String("aaa");
+
+    System.out.println(s == s2);
+  }
+}
---
+++ java-lang/app/src/main/java/study/lang/string/Test02.java
@@ -0,0 +1,10 @@
+package study.lang.string;
+
+public class Test02 {
+  public static void main(String[] args) {
+    String s = "aaa";
+    String s2 = "aaa";
+
+    System.out.println(s == s2);
+  }
+}
