8ad7174c93b7a7c9437913db0450e86615e29fa2
chocolithm <kys8336@gmail.com>
2024-07-08 16:28:02 +0900

    myapp2 HelpCommand 추가

--- myapp2/app/src/main/java/bitcamp/myapp/command/Command.java
+++ myapp2/app/src/main/java/bitcamp/myapp/command/Command.java
@@ -2,10 +2,4 @@ package bitcamp.myapp.command;
 
 public interface Command {
   void execute();
-
-  void printMenus(String menuTitle, String[] menus);
-
-  String getMenuTitle(int menuNo, String[] menus);
-
-  boolean isValidateMenu(int menuNo, String[] menus);
 }
