a1e548b0a7c130b9a763ec80db8ed569cdf022d2
chocolithm <kys8336@gmail.com>
2024-10-14 12:34:07 +0900

    myapp WebInitializer 적용

--- myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -1,44 +1,16 @@
 package bitcamp.myapp.config;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import javax.sql.DataSource;
-
-@ComponentScan("bitcamp.myapp")
+@ComponentScan("bitcamp.myapp.controller")
 @EnableWebMvc // WebMVC 관련기능 처리코드를 활성화
-@PropertySource({
-    "classpath:config/jdbc.properties",
-    "file:${user.home}/config/ncp.properties"
-})
-@EnableTransactionManagement // @Transactional 이 붙은 메서드가 붙은 클래스는 Proxy 를 만들어라
-@MapperScan("bitcamp.myapp.dao") // 인터페이스가 들어있는 패키지 위치
 public class AppConfig {
-
-  ApplicationContext appCtx;
-
-  public AppConfig(ApplicationContext appCtx) {
-    this.appCtx = appCtx;
-
-    // AWS 경고 메시지 로깅 비활성화
-    System.setProperty("aws.java.v1.disableDeprecationAnnouncement", "true");
-  }
-
   @Bean
   public ViewResolver viewResolver() {
     InternalResourceViewResolver vr = new InternalResourceViewResolver();
@@ -50,36 +22,5 @@ public class AppConfig {
   @Bean
   public MultipartResolver multipartResolver() {
     return new StandardServletMultipartResolver();
-  }
-
-  @Bean
-  public DataSource dataSource(
-      @Value("${jdbc.driver}") String jdbcDriver,
-      @Value("${jdbc.url}") String jdbcUrl,
-      @Value("${jdbc.username}") String jdbcUsername,
-      @Value("${jdbc.password}") String jdbcPassword) {
-
-    DriverManagerDataSource ds = new DriverManagerDataSource();
-    ds.setDriverClassName(jdbcDriver);
-    ds.setUrl(jdbcUrl);
-    ds.setUsername(jdbcUsername);
-    ds.setPassword(jdbcPassword);
-
-    return ds;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(DataSource ds) {
-    return new DataSourceTransactionManager(ds);
-  }
-
-  @Bean
-  public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception {
-    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-    factoryBean.setDataSource(ds);
-    factoryBean.setTypeAliasesPackage("bitcamp.myapp.vo");
-    factoryBean.setMapperLocations(appCtx.getResources("classpath:mappers/*Mapper.xml"));
-
-    return factoryBean.getObject();
   }
 }
