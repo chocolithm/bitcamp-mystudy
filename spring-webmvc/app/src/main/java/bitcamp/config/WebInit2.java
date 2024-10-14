61c3e28dce0bd014ec8d087483421ca700710173
chocolithm <kys8336@gmail.com>
2024-10-14 11:36:34 +0900

    spring-webmvc WebIntializer 연습

--- spring-webmvc/app/src/main/java/bitcamp/config/WebInit2.java
+++ spring-webmvc/app/src/main/java/bitcamp/config/WebInit2.java
@@ -2,14 +2,26 @@ package bitcamp.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import org.springframework.web.context.WebApplicationContext;
 
-public class WebInit1 /*implements WebApplicationInitializer*/ {
+public class WebInit2 /*extends AbstractContextLoaderInitializer*/ {
 
-  // @Override
+  //@Override
+  protected WebApplicationContext createRootApplicationContext() {
+    // ContextLoaderListener가 사용할 IoC Container를 리턴한다.
+    // 만약 IoC Container를 리턴하지 않으면
+    // ContextLoaderListener는 만들어지지 않는다.
+    return null;
+  }
+
+
+  //@Override
   public void onStartup(ServletContext servletContext) throws ServletException {
-    System.out.println("WebInit1.onStartup() 호출됨!");
+    System.out.println("WebInit2.onStartup() 호출됨!");
 
     /*
+    super.onStartup(servletContext);
+
     AnnotationConfigWebApplicationContext iocContainer = new AnnotationConfigWebApplicationContext();
     iocContainer.setServletContext(servletContext);
     iocContainer.register(AppConfig.class);
@@ -21,7 +33,6 @@ public class WebInit1 /*implements WebApplicationInitializer*/ {
 
     options.addMapping("/app/*");
      */
-
   }
 
 }
